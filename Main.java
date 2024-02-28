9d1bbb89d74537c1bdff4384f8d5a2b87d3ab400
henriquerlara <henriquerlara@outlook.com>
2024-02-28 12:27:41 -0300

    início visual do menu CRUD

--- Main.java
+++ Main.java
@@ -3,8 +3,7 @@ import java.util.*;
 
 class Main {
 
-    public static void main(String data[]) throws FileNotFoundException{
-
+    public static void main(String data[]) throws FileNotFoundException {
         FileOutputStream fos;
         DataOutputStream dos;
         FileInputStream fis;
@@ -12,97 +11,115 @@ class Main {
         Scanner scanner = new Scanner(System.in);
 
         try {
-            // Opening original data
-            File file = new File("dataanime.tsv");
-            RandomAccessFile raf = new RandomAccessFile(file, "r");
+            System.out.println("Escolha uma opção:");
+            System.out.println("1. Realizar a carga da base de dados selecionada");
+            System.out.println("2. Ler ID para imprimir informações do objeto");
+            System.out.print("Opção: ");
+            int option = scanner.nextInt();
 
-            // Preparing input path
-            fos = new FileOutputStream("anime.db");
-            dos = new DataOutputStream(fos);
+            switch (option) {
+                case 1:
+                    // Código para realizar a carga da base de dados selecionada
+                    // Aqui você pode colocar o código que estava dentro do try antes
 
-            // Skip header
-            String header = raf.readLine();
+                    // Opening original data
+                    File file = new File("dataanime.tsv");
+                    RandomAccessFile raf = new RandomAccessFile(file, "r");
 
-            // Receive inputs
-            String line;
-            while ((line = raf.readLine()) != null) {
-                String[] values = line.split("\t");
+                    // Preparing input path
+                    fos = new FileOutputStream("anime.db");
+                    dos = new DataOutputStream(fos);
 
-                Anime anime = new Anime();
+                    // Skip header
+                    String header = raf.readLine();
 
-                // Setting values
-                anime.setTitle(values[0]);
-                if (!values[1].equals("-"))
-                    anime.setId(Integer.parseInt(values[1]));
-                if (!values[2].equals("-"))
-                    anime.setEpisodes(Integer.parseInt(values[2]));
-                anime.setStatus(values[3]);
-                if (!values[4].equals("-"))
-                    anime.setStartAiring(anime.parseDateString(values[4]).getTime());
-                if (!values[5].equals("-"))
-                    anime.setEndAiring(anime.parseDateString(values[5]).getTime());
-                anime.setStartingSeason(values[6]);
-                anime.setBroadcastTime(values[7]);
-                anime.setProducers(values[8].split(","));
-                anime.setLicensors(values[9].split(","));
-                anime.setStudios(values[10].split(","));
-                anime.setSources(values[11]);
-                anime.setGenres(values[12].split(","));
-                anime.setDuration(values[13]);
-                anime.setRating(values[14]);
-                if (!values[15].equals("-"))
-                    anime.setScore(Float.parseFloat(values[15]));
-                if (!values[16].equals("-"))
-                    anime.setScoredBy(Integer.parseInt(values[16]));
-                if (!values[17].equals("-"))
-                    anime.setNumOfMembers(Integer.parseInt(values[17]));
-                if (!values[18].equals("-"))
-                    anime.setNumOfFavorites(Integer.parseInt(values[18]));
-                anime.setDescription(values[19]);
-                anime.setType(values[20]);
+                    // Receive inputs
+                    String line;
+                    while ((line = raf.readLine()) != null) {
+                        String[] values = line.split("\t");
 
-                byte[] ba;
-                ba = anime.toByteArray();
-                dos.writeInt(ba.length);
-                dos.write(ba);
-                System.out.println(anime.toString());
-            }
-            raf.close();
-            
-            // Fechar o fluxo de saída após terminar de escrever
-            dos.close();
-            fos.close();
+                        Anime anime = new Anime();
 
-            // Começar read para achar id
-            int idBuscado = scanner.nextInt(); //id que o usuário deseja procurar
+                        // Setting values
+                        anime.setTitle(values[0]);
+                        if (!values[1].equals("-"))
+                            anime.setId(Integer.parseInt(values[1]));
+                        if (!values[2].equals("-"))
+                            anime.setEpisodes(Integer.parseInt(values[2]));
+                        anime.setStatus(values[3]);
+                        if (!values[4].equals("-"))
+                            anime.setStartAiring(anime.parseDateString(values[4]).getTime());
+                        if (!values[5].equals("-"))
+                            anime.setEndAiring(anime.parseDateString(values[5]).getTime());
+                        anime.setStartingSeason(values[6]);
+                        anime.setBroadcastTime(values[7]);
+                        anime.setProducers(values[8].split(","));
+                        anime.setLicensors(values[9].split(","));
+                        anime.setStudios(values[10].split(","));
+                        anime.setSources(values[11]);
+                        anime.setGenres(values[12].split(","));
+                        anime.setDuration(values[13]);
+                        anime.setRating(values[14]);
+                        if (!values[15].equals("-"))
+                            anime.setScore(Float.parseFloat(values[15]));
+                        if (!values[16].equals("-"))
+                            anime.setScoredBy(Integer.parseInt(values[16]));
+                        if (!values[17].equals("-"))
+                            anime.setNumOfMembers(Integer.parseInt(values[17]));
+                        if (!values[18].equals("-"))
+                            anime.setNumOfFavorites(Integer.parseInt(values[18]));
+                        anime.setDescription(values[19]);
+                        anime.setType(values[20]);
 
-            fis = new FileInputStream("anime.db");
-            dis = new DataInputStream(fis);
-            int recordSize;
-            boolean found = false;
+                        byte[] ba;
+                        ba = anime.toByteArray();
+                        dos.writeInt(ba.length);
+                        dos.write(ba);
+                        System.out.println(anime.toString());
+                    }
+                    raf.close();
 
-            while (dis.available() > 0) {
-                recordSize = dis.readInt();
-                byte[] recordData = new byte[recordSize];
-                dis.readFully(recordData);
-
-                Anime anime = new Anime();
-                anime.fromByteArray(recordData);
-
-                if (anime.getId() == idBuscado) {
-                    System.out.println(anime.toString());
-                    found = true;
+                    // Fechar o fluxo de saída após terminar de escrever
+                    dos.close();
+                    fos.close();
                     break;
-                }
-            }
+                    
+                case 2:
+                    // Código para ler um ID e imprimir as informações do objeto
+                    System.out.print("Digite o ID do anime que deseja buscar: ");
+                    int idBuscado = scanner.nextInt(); // id que o usuário deseja procurar
 
-            if (!found) {
-                System.out.println("Anime com ID " + idBuscado + " não encontrado.");
-            }
+                    fis = new FileInputStream("anime.db");
+                    dis = new DataInputStream(fis);
+                    int recordSize;
+                    boolean found = false;
 
-            // fechar os fluxos
-            fis.close();
-            dis.close();
+                    while (dis.available() > 0) {
+                        recordSize = dis.readInt();
+                        byte[] recordData = new byte[recordSize];
+                        dis.readFully(recordData);
+
+                        Anime anime = new Anime();
+                        anime.fromByteArray(recordData);
+
+                        if (anime.getId() == idBuscado) {
+                            System.out.println(anime.toString());
+                            found = true;
+                            break;
+                        }
+                    }
+
+                    if (!found) {
+                        System.out.println("Anime com ID " + idBuscado + " não encontrado.");
+                    }
+
+                    // fechar os fluxos
+                    fis.close();
+                    dis.close();
+                    break;
+                default:
+                    System.out.println("Opção inválida.");
+            }
 
         } catch (FileNotFoundException e) {
             System.err.println("File not found: " + e.getMessage());
