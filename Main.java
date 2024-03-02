b925b9339ab99ce247ff11f6cc2ebae18d122b80
niet <antonionetodev@yahoo.com>
2024-03-02 18:53:39 -0300

    add: update, better expection handling, new interface

--- Main.java
+++ Main.java
@@ -13,14 +13,8 @@ class Main {
 
         try {
 
-            // Guia visual do crud
-            System.out.println("Escolha uma tarefa:");
-            System.out.println("1. Carregar base de dados original");
-            System.out.println("2. Criar novo registro (CREATE)");
-            System.out.println("3. Busca por ID (READ)");
-            System.out.println("4. Deletar um registro por ID (DELETE)");
-            System.out.println("5. Update em um registro por ID (UPDATE)");
-            System.out.print("Tarefa: ");
+            // Função da interface base do programa
+            Anime.animeInterface();
             option = scanner.nextInt();
 
             switch (option) {
@@ -37,7 +31,7 @@ class Main {
                     dos = new DataOutputStream(fos);
                     RandomAccessFile rafAnimeDb = new RandomAccessFile("anime.db", "rw");
 
-                    // Pulando header
+                    // Pulando header do arquivo original
                     String header = raf.readLine();
 
                     dos.writeInt(0); // reservando os 4 primeiros bytes para o último id
@@ -59,7 +53,6 @@ class Main {
                     }
 
                     raf.close();
-                    // Fechar o fluxo
                     dos.close();
                     fos.close();
                     rafAnimeDb.close();
@@ -79,7 +72,7 @@ class Main {
                         ra.seek(ra.length());
 
                         // Gravar o novo registro
-                        ra.writeBoolean(false); // Lapide
+                        ra.writeBoolean(false); // lapideRead
                         byte[] recordData = a.toByteArray();
                         ra.writeInt(recordData.length); // Tamanho do registro
                         ra.write(recordData); // Dados do registro
@@ -102,21 +95,21 @@ class Main {
                     dis = new DataInputStream(fis);
                     int idBuscado = scanner.nextInt();
 
-                    int recordSize;
-                    boolean lapide;
-                    boolean found = false;
+                    int tamRegistroRead;
+                    boolean lapideRead;
+                    boolean encontradoRead = false;
                     dis.readInt();
 
-                    while (dis.available() > 0) {
-                        lapide = dis.readBoolean();
-                        if (lapide == true) {
-                            recordSize = dis.readInt();
-                            dis.skipBytes(recordSize);
+                    while (dis.available() > 0) { // Enquanto existirem bytes para serem lidos
+                        lapideRead = dis.readBoolean();
+                        if (lapideRead == true) {
+                            tamRegistroRead = dis.readInt();
+                            dis.skipBytes(tamRegistroRead);
                             continue;
                         }
                         // Ler indicador de tamanho
-                        recordSize = dis.readInt();
-                        byte[] recordData = new byte[recordSize];
+                        tamRegistroRead = dis.readInt();
+                        byte[] recordData = new byte[tamRegistroRead];
                         // Ler no arquivo vetor de bytes respectivo
                         dis.readFully(recordData);
 
@@ -126,15 +119,14 @@ class Main {
 
                         if (anime.getId() == idBuscado) {
                             System.out.println(anime.toString());
-                            found = true;
+                            encontradoRead = true;
                             break;
                         }
                     }
 
-                    if (!found) {
+                    if (!encontradoRead) {
                         System.out.println("Anime com ID " + idBuscado + " não encontrado.");
                     }
-                    // Fechar os fluxos
                     fis.close();
                     dis.close();
                     break;
@@ -145,23 +137,23 @@ class Main {
                     RandomAccessFile raDel = new RandomAccessFile("anime.db", "rw");
                     int idRemover = scanner.nextInt();
 
-                    int recordSize2;
-                    boolean lapide2;
-                    boolean found2 = false;
+                    int tamRegistroDel;
+                    boolean lapideDel;
+                    boolean encontradoDel = false;
                     raDel.readInt(); // ler ultimo id
 
                     while (raDel.getFilePointer() < raDel.length()) {
-                        long posicaoLapide = raDel.getFilePointer(); // guarda a posição da lapide
-                        lapide2 = raDel.readBoolean();
+                        long posicaoLapide = raDel.getFilePointer(); // guarda a posição da lapideRead
+                        lapideDel = raDel.readBoolean();
 
-                        if (lapide2 == true) {
-                            recordSize2 = raDel.readInt();
-                            raDel.skipBytes(recordSize2);
+                        if (lapideDel == true) {
+                            tamRegistroDel = raDel.readInt();
+                            raDel.skipBytes(tamRegistroDel);
                             continue;
                         }
                         // Ler indicador de tamanho
-                        recordSize = raDel.readInt();
-                        byte[] recordData = new byte[recordSize];
+                        tamRegistroRead = raDel.readInt();
+                        byte[] recordData = new byte[tamRegistroRead];
                         // Ler no arquivo vetor de bytes respectivo
                         raDel.readFully(recordData);
 
@@ -174,42 +166,41 @@ class Main {
                             raDel.writeBoolean(true);
                             System.out.println(
                                     "O anime " + anime.getTitle() + ", com ID " + idRemover + ", foi removido");
-                            found2 = true;
+                            encontradoDel = true;
                             break;
                         }
                     }
 
-                    if (!found2) {
+                    if (!encontradoDel) {
                         System.out.println("Anime com ID " + idRemover + " não encontrado.");
                     }
                     raDel.close();
                     break;
 
                 // ATUALIZAR REGISTRO POR ID
-                // Essa versão coloca lápide no velho e cria um novo registro no final com o mesmo id do velho
                 case 5:
                     System.out.print("Digite o ID do anime que deseja atualizar: ");
                     int idAtualizar = scanner.nextInt();
                     RandomAccessFile raUpd = new RandomAccessFile("anime.db", "rw");
                     boolean encontrado = false;
 
-                    int recordSize3;
-                    boolean lapide3;
+                    int tamRegistroAtt;
+                    boolean lapideAtt;
 
-                    raUpd.readInt(); // lê o último ID
+                    raUpd.readInt(); // Lê o último ID pra mover o ponteiro pra parte significante
 
-                    while (raUpd.getFilePointer() < raUpd.length()) {
-                        long posicaoLapide = raUpd.getFilePointer(); // guarda a posição da lapide
-                        lapide3 = raUpd.readBoolean();
+                    while (raUpd.getFilePointer() < raUpd.length()) { // Enquanto o ponteiro não atingir o valor máximo no contexto
+                        long posicaoLapide = raUpd.getFilePointer(); // Guarda a posição da lapide
+                        lapideAtt = raUpd.readBoolean(); // Guarda o valor da lapide
 
-                        if (lapide3 == true) {
-                            recordSize3 = raUpd.readInt();
-                            raUpd.skipBytes(recordSize3);
+                        if (lapideAtt == true) {
+                            tamRegistroAtt = raUpd.readInt();
+                            raUpd.skipBytes(tamRegistroAtt);
                             continue;
                         }
                         // Ler indicador de tamanho
-                        recordSize3 = raUpd.readInt();
-                        byte[] recordData = new byte[recordSize3];
+                        tamRegistroAtt = raUpd.readInt();
+                        byte[] recordData = new byte[tamRegistroAtt];
                         // Ler no arquivo vetor de bytes respectivo
                         raUpd.readFully(recordData);
 
@@ -220,20 +211,41 @@ class Main {
                         if (anime.getId() == idAtualizar) {
                             Anime novoAnime = Anime.promptUser(idAtualizar); // Solicita ao usuário os novos dados
                             novoAnime.setId(idAtualizar); // Define o mesmo ID
-
-                            raUpd.seek(posicaoLapide);
-                            raUpd.writeBoolean(true); // Marca o registro original como removido
-
-                            raUpd.seek(raUpd.length()); // Vai para o final do arquivo para adicionar o novo registro
-                            raUpd.writeBoolean(false); // Lapide
+                        
                             byte[] novoRecordData = novoAnime.toByteArray();
-                            raUpd.writeInt(novoRecordData.length); // Tamanho do registro
-                            raUpd.write(novoRecordData); // Dados do registro
-
-                            System.out.println("Registro atualizado com sucesso.");
-                            encontrado = true;
+                            int novoRecordSize = novoRecordData.length;
+                        
+                            // Se o novo registro for maior que o registro antigo
+                            if (novoRecordSize > tamRegistroAtt) {
+                                // Marcar o original como removido
+                                raUpd.seek(posicaoLapide);
+                                raUpd.writeBoolean(true);
+                        
+                                // Move pointeiro pro fim do arquivo
+                                raUpd.seek(raUpd.length());
+                        
+                                // Escrever o novo registro
+                                raUpd.writeBoolean(false); // Lapide
+                                raUpd.writeInt(novoRecordSize); // Tamanho do registro
+                                raUpd.write(novoRecordData); // Novo anime
+                        
+                                System.out.println("Registro atualizado com sucesso, movido para o fim do arquivo.");
+                                encontrado = true;
+                            } else { // Se o novo registro for <= registro antigo
+                                // Move o ponteiro para o inicio do registro antigo
+                                raUpd.seek(posicaoLapide);
+                                // Escreve os padrões de existência do registro
+                                raUpd.writeBoolean(false);
+                                raUpd.writeInt(tamRegistroAtt);
+                                // Escreve o novo registro
+                                raUpd.write(novoRecordData);
+                        
+                                System.out.println("Registro atualizado com sucesso, mantido no mesmo lugar.");
+                                encontrado = true;
+                            }
                             break;
                         }
+                        
                     }
 
                     if (!encontrado) {
@@ -244,10 +256,12 @@ class Main {
                     break;
 
                 default:
-                    System.out.println("Opção inválida.");
+                    System.out.println("Saíndo...");
 
                     scanner.close();
             }
+
+        // Exceptions
 
         } catch (FileNotFoundException e) {
             System.err.println("Arquivo não encontrado: " + e.getMessage());
@@ -255,6 +269,8 @@ class Main {
             System.err.println("Erro ao ler arquivo: " + e.getMessage());
         } catch (NumberFormatException e) {
             System.err.println("Erro de formatação numérico: " + e.getMessage());
+        } catch (InputMismatchException e) {
+            System.err.println("Esperado um inteiro.");
         }
     }
 }
